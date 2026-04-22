1e5c41ad2e94412cfd5e901f1cc71c69985b9452
Mhmdulinnuha <muhamadulinnuha653@gmail.com>
2026-04-22 21:14:00 +0700

    membuat product repository go

---
+++ backend/repositories/product_repository.go
@@ -0,0 +1,44 @@
+package repositories
+
+import (
+"github.com/Mhmdulinnuha/uts1123150002/backend/config"
+"github.com/Mhmdulinnuha/uts1123150002/backend/models"
+)
+type ProductRepository struct{}
+func NewProductRepository() *ProductRepository {
+return &ProductRepository{}
+}
+
+func (r *ProductRepository) FindAll(page, limit int, category string)([]models.Product, int64, error) {
+var products []models.Product
+var total int64
+query := config.DB.Model(&models.Product{}).Where("is_active = ?", true)
+
+if category != "" {
+query = query.Where("category = ?", category)
+}
+
+query.Count(&total)
+
+offset := (page - 1) * limit
+result := query.Offset(offset).Limit(limit).Find(&products)
+return products, total, result.Error
+}
+
+func (r *ProductRepository) FindByID(id uint) (*models.Product, error) {
+var product models.Product
+result := config.DB.First(&product, id)
+return &product, result.Error
+}
+
+func (r *ProductRepository) Create(product *models.Product) error {
+return config.DB.Create(product).Error
+}
+
+func (r *ProductRepository) Update(product *models.Product) error {
+return config.DB.Save(product).Error
+}
+
+func (r *ProductRepository) Delete(id uint) error {
+return config.DB.Delete(&models.Product{}, id).Error
+}
